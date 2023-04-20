f5727cb98baecdfbbb13207508fc09f6d9054773
Daniel <jorgedanielsaini@gmail.com>
2023-04-20 19:27:53 -0300

    corrección de estilos y agregado del login

--- botanica/tp_botanico/tp_botanico/settings.py
+++ botanica/tp_botanico/tp_botanico/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'publica'
+    'publica',
+    'login_registro'
 ]
 
 MIDDLEWARE = [
