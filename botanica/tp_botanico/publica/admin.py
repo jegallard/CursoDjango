43eda5d4d6b8906a51954654ee235edfc58b5dd1
jegallard <jegallard@gmail.com>
2023-05-27 17:16:18 -0300

    alta de modelos en publica

--- botanica/tp_botanico/publica/admin.py
+++ botanica/tp_botanico/publica/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cliente
+from .models import Planta
+from .models import Categoria
+from .models import Producto
+from .models import Kits
 
 # Register your models here.
+admin.site.register(Cliente)
